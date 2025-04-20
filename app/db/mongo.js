bcdcadf2deaa851e77666c01c550387aeb33d1cf
Utkarsh Srivastava <utkarshutt2706@gmail.com>
2025-04-20 20:17:27 +0530

    Update mongo DB name

--- app/db/mongo.js
+++ app/db/mongo.js
@@ -6,10 +6,12 @@ dotenv.config();
 let isConnected = false;
 
 export const createMongoConnection = async () => {
-  const mongoUri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DB_NAME}`;
+  const mongoUri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_NAME}`;
   try {
     isConnected = false;
-    await mongoose.connect(mongoUri);
+    await mongoose.connect(mongoUri, {
+      dbName: process.env.MONGO_DB_NAME,
+    });
     isConnected = true;
     console.log('connected to mongo on URI: ', mongoUri);
   } catch (error) {

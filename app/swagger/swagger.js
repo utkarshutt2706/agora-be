d22d1ca414faf7552e15bf7edb82a0c89366431c
Utkarsh Srivastava <utkarshutt2706@gmail.com>
2025-07-21 23:28:30 +0530

    Chore: Update swagger base URL

--- app/swagger/swagger.js
+++ app/swagger/swagger.js
@@ -1,20 +1,22 @@
 import swaggerJSDoc from 'swagger-jsdoc';
 
 const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Chat App API',
-      version: '1.0.0',
-      description: 'A simple real-time chat app API',
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Chat App API',
+            version: '1.0.0',
+            description: 'A simple real-time chat app API',
+        },
+        servers: [
+            {
+                url:
+                    process.env.BE_BASE_URL ||
+                    `http://localhost:${process.env.PORT || 3000}`,
+            },
+        ],
     },
-    servers: [
-      {
-        url: `http://localhost:${process.env.PORT || 3000}`, // Adjust port if needed
-      },
-    ],
-  },
-  apis: ['./app/routes/*.js'], // Files containing Swagger annotations
+    apis: ['./app/routes/*.js'], // Files containing Swagger annotations
 };
 
 const swaggerSpec = swaggerJSDoc(options);
